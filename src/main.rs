5f80644a0cc680ad0122178fec66a57fe30cfd87
Paul Fresnel <paul.fresnel1@gmail.com>
2023-12-07 17:21:18 +0100

    Fixed last intruction

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ fn main() {
 }
 
 fn fizz_buzz(n:u32) -> (){
+    let mut times = 0;
     for i in 1..=n{
         match i{
-            i if &i % 15 == 0 => println!("fizz buzz"),
+            i if &i % 15 == 0 => {
+                times += 1;
+                println!("fizz buzz")
+            },
             i if &i % 3 == 0 => println!("fizz"),
             i if &i % 5 == 0 => println!("buzz"),
             _ => ()
         }
     }
+    println!("Fizz Buzz recurrences: {}", times);
 }
 
